b3c416b28dcfc2d9234939bb0ac84b7ab40779b4
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-11-09 19:29:32 +0100

    Add homepage Session tab

--- pages/SessionHomePage.tsx
+++ pages/SessionHomePage.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {Button, colors} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
 
 const SessionHomePage = () => {
+  const {t} = useTranslation();
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View testID="sessionHomePage">
-        <Text>Session Home Page</Text>
+        <Button
+          titleStyle={styles.buttonTitle}
+          iconRight
+          type="clear"
+          icon={
+            <Icon name="add" size={50} type="material" color={colors.primary} />
+          }
+          title={t('createSession')}
+        />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    fontSize: 20,
+  },
+});
 
 export default SessionHomePage;
