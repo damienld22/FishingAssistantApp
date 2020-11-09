5e2c010faa7f4bba2b0684515f8ddd582ac68a68
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-11-09 22:50:16 +0100

    Improve UI session home page

--- pages/SessionHomePage.tsx
+++ pages/SessionHomePage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {View, SafeAreaView, StyleSheet} from 'react-native';
-import {Button, colors} from 'react-native-elements';
-import {Icon} from 'react-native-elements';
+import IconWithBottomText from '../components/shared/IconWithBottomText';
 
 const SessionHomePage = () => {
   const {t} = useTranslation();
@@ -10,14 +9,10 @@ const SessionHomePage = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View testID="sessionHomePage">
-        <Button
-          titleStyle={styles.buttonTitle}
-          iconRight
-          type="clear"
-          icon={
-            <Icon name="add" size={50} type="material" color={colors.primary} />
-          }
-          title={t('createSession')}
+        <IconWithBottomText
+          icon="plus-circle"
+          type="font-awesome-5"
+          text={t('createSession')}
         />
       </View>
     </SafeAreaView>
