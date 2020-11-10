47824d460e083412fd1c3c1a3010e768b2d9a21a
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-11-10 21:07:44 +0100

    Improve useRequestHandlerHook

--- hooks/RequestHandler.tsx
+++ hooks/RequestHandler.tsx
@@ -1,18 +1,44 @@
 import {useState} from 'react';
-import axios from 'axios';
+import axios, {AxiosRequestConfig} from 'axios';
+import {BASE_URL} from '../utils';
 
 const useRequestHandler = () => {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
 
-  const handleRequest = (request: string) => {
+  const handleRequest = ({
+    url,
+    method,
+    token,
+  }: {
+    url: AxiosRequestConfig['url'];
+    method: AxiosRequestConfig['method'];
+    token?: string;
+  }) => {
     setLoading(true);
     setError(null);
     setData(null);
 
-    return axios
-      .get(request)
+    const headers = {} as AxiosRequestConfig['headers'];
+
+    if (requestNeedTokenAuthorization(token)) {
+      headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (needApplicationJsonContentType(method)) {
+      headers['Content-Type'] = 'application/json';
+    }
+
+    return axios({
+      baseURL: BASE_URL,
+      method,
+      url,
+      headers:
+        method?.toLowerCase() === 'post'
+          ? {Authorization: `Bearer ${token}`}
+          : {},
+    })
       .then(({data: result}) => setData(result))
       .catch((err) => setError(err.response.status))
       .finally(() => setLoading(false));
@@ -20,5 +46,13 @@ const useRequestHandler = () => {
 
   return {isLoading, error, data, handleRequest};
 };
+
+function requestNeedTokenAuthorization(token?: string) {
+  return !!token;
+}
+
+function needApplicationJsonContentType(method?: AxiosRequestConfig['method']) {
+  return method && ['post', 'put', 'patch'].includes(method?.toLowerCase());
+}
 
 export default useRequestHandler;
---
+++ utils.tsx
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://241.ip-92-222-68.eu/';
